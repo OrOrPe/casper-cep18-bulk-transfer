9d4af8d13df3165f2799952fc8279863298ddae6
ihor <ihor.burlachenko@gmail.com>
2021-09-02 16:40:34 +0200

    Added arguments and simple validation to the script

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
-# sendrewards
+# Casper rewards sender
 
-## How to use
+This script sends bulk transfers on the Casper blockchain network reading the recipients and amounts from a CSV file in the format ```recipient_public_key,amount_in_motes```. 
 
-1. Replace `sampledata.csv` with your own CSV file containing lines of recipients and amounts in `motes`, following the same structure (`address,motes`) -- IMPORTANT: amounts should be provided in `motes`, not `CSPR` with decimals. 
-2. In `sendrewards.sh`, replace the path to your private key with the path to the private key belonging to the account you plan to send your tokens from
-3. In `sendrewards.sh`, replace the CHAINSPEC value with the name of the network you're sending on, e.g. `casper`
-4. In `sendrewards.sh`, replace the NODE_ADDRESS value with the URL to the Casper Node you're sending your deploys too (can leave as is if sending to localhost)
-5. `chmod +x sendrewards.sh` to ensure it's executable
-6. Triple check everything, as there is no failsafe once you start sending.
+## Usage
+
+```
+./sendrewards.sh --file=~/rewards.csv --keys-path=~/my-casper-keys
+```
+
+Triple check everything, as there is no failsafe once you start sending.
+
+## Disclaimer
 
 Provided as is and with no warranties, or guarantees to be bug-free.
--- sendrewards.sh
+++ sendrewards.sh
@@ -1,24 +1,89 @@
 #!/bin/bash
 
-FILE="./sampledata.csv"
-PRIVATE_KEY="/etc/casper/validator_keys/secret_key.pem"
-CHAINSPEC="casper-test"
-NODE_ADDRESS="http://127.0.0.1:7777"
+print_usage () {
+  echo "USAGE:"
+  echo "  sendrewards.sh [ARGUMENTS]"
+  echo
+  echo "ARGUMENTS:"
+  echo "  --node-address  Casper node to run RPC requests against (default: 127.0.0.1)"
+  echo "  --file          CSV file in format <recipient_public_key>,<rewards_in_motes>"
+  echo "  --keys-path     Path to the directory with Casper keys of the sender account"
+  echo
+  echo "EXAMPLE:"
+  echo "  sendrewards.sh --file=~/rewards.csv --keys-path=~/casper-keys"
+  echo
+  echo "DEPENDENCIES:"
+  echo "  casper-client    To make RPC requests to the network"
+  echo "  jq               To parse RPC responses"
+  echo
+  echo "DISCLAIMER:"
+  echo "Provided as is and with no warranties, or guarantees to be bug-free."
+}
 
+ensure_has_installed () {
+  HAS_INSTALLED=$(which "$1")
+  if [ "$HAS_INSTALLED" = "" ]; then
+    echo "Please install $1"
+    exit 1
+  fi
+}
+
+ensure_has_installed "casper-client"
+ensure_has_installed "jq"
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --node-address=*)
+      NODE_ADDRESS="${1#*=}"
+      ;;
+    --file=*)
+      FILE="${1#*=}"
+      ;;
+    --keys-path=*)
+      KEYS_PATH="${1#*=}"
+      ;;
+    *)
+      print_usage; exit 1
+      ;;
+  esac
+  shift
+done
+
+if [ -z ${NODE_ADDRESS+x} ]; then NODE_ADDRESS=127.0.0.1; fi
+if [ -z ${FILE+x} ]; then print_usage; exit 1; fi
+if [ -z ${KEYS_PATH+x} ]; then print_usage; exit 1; fi
+
+CHAIN_NAME=$(curl -s http://$NODE_ADDRESS:8888/status | jq -r '.chainspec_name')
+
+# Validate the input
+LINE=1
+while IFS="," read PUBLIC_KEY MOTES ; do
+  if (( ${#PUBLIC_KEY} != 66 )) && (( ${#PUBLIC_KEY} != 68 )); then
+    echo "Please check if you put a valid recipient public key on the line $LINE"
+    exit 2
+  fi
+
+  if (( ${#MOTES} < 9 )); then
+    echo "Please check if you put a correct rewards amount on the line $LINE. It is not possible to send less than 2.5 CSPR."
+    exit 2
+  fi
+
+  ((LINE++))
+done < $FILE
+
+# Send rewards
 TRANSFER_ID=1
-
-while IFS="," read address motes ; do
-    echo Transfer $TRANSFER_ID - $address is getting $motes motes
+while IFS="," read PUBLIC_KEY MOTES ; do
+    echo Transfer $TRANSFER_ID - $PUBLIC_KEY is getting $MOTES motes
 
     sudo -u casper casper-client transfer \
-        --chain-name "$CHAINSPEC" \
-        --node-address "$NODE_ADDRESS" \
-        --secret-key $PRIVATE_KEY \
+        --chain-name "$CHAIN_NAME" \
+        --node-address http://$NODE_ADDRESS:7777 \
+        --secret-key "$KEYS_PATH/secret_key.pem" \
         --transfer-id "$TRANSFER_ID" \
-        -a $motes \
-        -t "$address" \
+        -a $MOTES \
+        -t "$PUBLIC_KEY" \
         -p 10000
 
     ((TRANSFER_ID++))
-
 done < $FILE
