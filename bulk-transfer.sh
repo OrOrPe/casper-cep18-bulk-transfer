df7faa581dd7c5600df1625255acaae5cb791281
OrPerets <or@rndrobin.com>
2023-06-14 14:47:12 -0700

    Bug fixes + production support

--- bulk-transfer.sh
+++ bulk-transfer.sh
@@ -64,15 +64,15 @@ if [ -z ${KEYS_PATH+x} ]; then print_usage; exit 1; fi
 if [ -z ${IN+x} ]; then print_usage; exit 1; fi
 if [ -z ${OUT+x} ]; then OUT=/dev/null; fi
 
-if [ $CASPER_ENV == "test" ]; then
+if [ "$CASPER_ENV" = "test" ]; then
   # Test Astro
   CEP18_CONTRACT_HASH=f71c3cb768825b0084ef78478ecbe0ea2d56f29e239ebc82ed0e16e7bb9b080e
   CHAIN_NAME="casper-test"
   NODE_ADDRESS=https://rpc.testnet.casperlabs.io/
-elif [ $CASPER_ENV == "prod" ]; then
+elif [ "$CASPER_ENV" = "prod" ]; then
   # Mainnet
-  CEP18_CONTRACT_HASH=NotDeployedYet
-  CHAIN_NAME="casper-net-1"
+  CEP18_CONTRACT_HASH=30a39954dfe9baa2a8ea5a1f87fcb1b5deeb2bfa8feac07ce2ce022a5e080fdc
+  CHAIN_NAME="casper"
   NODE_ADDRESS=http://3.14.161.135:7777/
   # Any of these could be used for mainnet
   # 3.14.161.135
@@ -91,10 +91,11 @@ while IFS="," read PUBLIC_KEY MOTES ; do
     exit 2
   fi
 
-  if (( ${#MOTES} < 9 )); then
-    echo "Please check if you put a correct rewards amount on the line $LINE. It is not possible to send less than 0.000000001 BOIN."
-    exit 2
-  fi
+ # Remove this test to have "0" transactions also in results summary
+ # if (( ${#MOTES} < 9 )); then
+ #   echo "Please check if you put a correct rewards amount on the line $LINE. It is not possible to send less than 0.000000001 BOIN."
+ #   exit 2
+ # fi
 
   ((LINE++))
 done < $IN
@@ -104,19 +105,22 @@ echo "public_key,motes,deploy_hash,transfer_id" > $OUT
 
 TRANSFER_ID=1
 while IFS="," read PUBLIC_KEY MOTES ; do
+  if (($MOTES > 0 )) ; then
+    DEPLOY_HASH=$(casper-client put-deploy \
+      --chain-name "$CHAIN_NAME" \
+      --node-address "$NODE_ADDRESS" \
+      --secret-key "$KEYS_PATH" \
+      --session-hash "hash-$CEP18_CONTRACT_HASH" \
+      --session-entry-point "transfer" \
+      --session-arg "recipient:key='account-hash-$PUBLIC_KEY'" \
+      --session-arg "transfer-id:u256='$TRANSFER_ID'" \
+      --session-arg "amount:u256='$MOTES'" \
+      --payment-amount "5000000000" | jq -r '.result | .deploy_hash')
+  else
+    DEPLOY_HASH="Zero Deal"
+  fi
 
-  DEPLOY_HASH=$(casper-client put-deploy \
-    --chain-name "$CHAIN_NAME" \
-    --node-address "$NODE_ADDRESS" \
-    --secret-key "$KEYS_PATH" \
-    --session-hash "hash-$CEP18_CONTRACT_HASH" \
-    --session-entry-point "transfer" \
-    --session-arg "recipient:key='account-hash-$PUBLIC_KEY'" \
-    --session-arg "transfer-id:u256='$TRANSFER_ID'" \
-    --session-arg "amount:u256='$MOTES'" \
-    --payment-amount "5000000000" | jq -r '.result | .deploy_hash')
-
-  echo "Transferred $MOTES motes to $PUBLIC_KEY (deploy hash $DEPLOY_HASH, transfer id $TRANSFER_ID)"
+  echo "Transferred $MOTES motes to $PUBLIC_KEY (deploy hash $DEPLOY_HASH, transfer id $TRANSFER_ID)";
   echo "$PUBLIC_KEY,$MOTES,$DEPLOY_HASH,$TRANSFER_ID" >> $OUT
 
   ((TRANSFER_ID++))
